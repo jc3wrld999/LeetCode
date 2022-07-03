66c54aa2c7bd2e3b92c7610a55160f579f1d2eb5
jc3wrld999 <61821641+jc3wrld999@users.noreply.github.com>
2022-07-03 14:37:52 +0900

    Time: 170 ms (63.23%), Space: 50.7 MB (88.24%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
@@ -7,20 +7,14 @@
  */
 var maxArea = function(h, w, hc, vc) {
     hc.sort((a,b) => a - b)
-    hc.push(h)
     vc.sort((a,b) => a - b)
-    vc.push(w)
-    let max_h = hc[0];
-    let max_v = vc[0];
-    for (let i = 0; i < hc.length - 1;i++) {
-
-        max_h = Math.max(hc[i+1]-hc[i], max_h)
-        console.log(max_h)
-
+    let max_h = Math.max(hc[0], h - hc[hc.length - 1]);
+    let max_v = Math.max(vc[0], w - vc[vc.length - 1]);
+    for (let i = 1; i < hc.length;i++) {
+        max_h = Math.max(hc[i]-hc[i - 1], max_h)
     }
-    for (let i = 0; i < vc.length - 1;i++) {
-        max_v = Math.max(vc[i+1]-vc[i], max_v)
-        console.log(max_v)
+    for (let i = 1; i < vc.length;i++) {
+        max_v = Math.max(vc[i]-vc[i - 1], max_v)
     }
     return BigInt(max_h) * BigInt(max_v) % 1000000007n
 };
