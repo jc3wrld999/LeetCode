2f62a5b1071b3b7f98fdfab1b6e9a6d9a61fb173
jc3wrld999 <61821641+jc3wrld999@users.noreply.github.com>
2022-07-04 09:48:37 +0900

    Time: 87 ms (48.90%), Space: 42.2 MB (38.69%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.js
+++ 376-wiggle-subsequence/376-wiggle-subsequence.js
@@ -7,14 +7,12 @@ var wiggleMaxLength = function(N) {
     let i = 1;
     while(N[i]===N[i-1]) i++;
     let up = N[i] < N[i-1]
-    console.log(false && false)
+
     let ans = 1
     
     for (; i < len; i++)
-        if ((up && N[i] < N[i-1]) || (!up && N[i] > N[i-1])) {
+        if ((up && N[i] < N[i-1]) || (!up && N[i] > N[i-1])) 
             up = !up, ans++
-            console.log(N[i])
-        }
             
     return ans
 
