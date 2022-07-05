d0a01955ab7c515be615cde8fd959d4a48b53be2
jc3wrld999 <61821641+jc3wrld999@users.noreply.github.com>
2022-07-05 10:26:13 +0900

    Time: 185 ms (61.41%), Space: 62.6 MB (24.09%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -2,19 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(N) {
-
-    if(N.length == 0)return 0;
-    N.sort((a,b) => a-b);
-    let count = 1, max = 0;
-
-    for(let i = 1;i<N.length;i++) {
-        if(N[i]-N[i-1] === 1) count++;
-        else if(N[i] !== N[i-1]){
-            console.log(N[i])
-            max = Math.max(max, count)
-            count = 1;
+var longestConsecutive = function(nums) {
+    let nmap = new Map(), ans = 0,
+        seen = new Uint32Array(nums.length)
+    for (let i = 0; i < nums.length; i++)
+        if (!nmap.has(nums[i])) nmap.set(nums[i], i)
+    for (let n of nums) {
+        let curr = n, count = 1
+        if (seen[nmap.get(curr)]) continue
+        while (nmap.has(curr+1)) {
+            let ix = nmap.get(++curr)
+            if (seen[ix]) {
+                count += seen[ix]
+                break
+            } else seen[ix] = 1, count++
         }
+        seen[nmap.get(n)] = count
+        ans = Math.max(ans, count)
     }
-    return Math.max(max, count)
+    return ans
 };
