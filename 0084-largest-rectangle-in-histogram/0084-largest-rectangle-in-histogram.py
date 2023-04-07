cfc96fb545943e98b67d1d3961869581f2f508e4
seung ju <jc3wrld999@gmail.com>
2023-04-07 09:53:43 +0900

    Time: 999 ms (51.62%), Space: 28.8 MB (51.00%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -6,13 +6,12 @@ class Solution:
         for i in range(N):
 
             # pop
-            if stack and stack[-1][1] > h[i]:
+            if stack and stack[-1][1] >= h[i]:
                 j = i
                 while stack and stack[-1][1] >= h[i]:
                     index, height = stack.pop()
                     m.append((i - index) * height)
                     j = index
-
                 stack.append((j, h[i]))
             else:
                 stack.append((i, h[i]))
