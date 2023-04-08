fd79c26542902502e6ed2a03f4e033a91f7defbc
seung ju <jc3wrld999@gmail.com>
2023-04-08 17:47:53 +0900

    Time: 1010 ms (8.83%), Space: 20.8 MB (7.40%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,23 @@
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        res = []
+        minH = []
+        for i, p in enumerate(points):
+            x = p[0]
+            y = p[1]
+            closet = x ** 2 + y ** 2
+            print(p, closet)
+            heapq.heappush(minH, (closet, i))
+        print(minH)
+        # 같은 값 골라내야함
+
+        # while minH[-1][0] == minC:
+        #     minC, minP = heapq.heappop(minH)
+        #     res.append([points[minP][0], points[minP][1]])
+        for i in range(k):
+            minC, minP = heapq.heappop(minH)
+            res.append([points[minP][0], points[minP][1]])
+        return res
+            
+        
