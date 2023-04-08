119402efa7c2b408a6735a3b8eaa25c278f6ba85
seung ju <jc3wrld999@gmail.com>
2023-04-08 09:53:37 +0900

    Time: 46 ms (58.66%), Space: 14.3 MB (99.35%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for m in matrix:
+            l = m[0]
+            h = m[-1]
+            if target in range(l, h+1):
+                for j in m:
+                    if j == target:
+                        return True
+                return False
+            else:
+                continue
+            # i = bisect_left(m, target)
+            # if m[i] == target:
+            #     return True
+        return False
+        
