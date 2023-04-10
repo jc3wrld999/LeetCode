d39cc342937bc4d2891f07496c161b0a4df6c55c
seung ju <jc3wrld999@gmail.com>
2023-04-10 13:54:49 +0900

    Time: 656 ms (65.62%), Space: 13.9 MB (84.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,7 +4,7 @@ class Solution:
         resLen = 0
 
         for i in range(len(s)):
-            # 짝수
+            # 홀수
             l, r = i, i
             # 같은 동안 루프문
             while l >= 0 and r < len(s) and s[l] == s[r]:
@@ -14,7 +14,7 @@ class Solution:
                     resLen = r - l + 1
                 l -= 1
                 r += 1
-            # 홀수
+            # 짝수
             l, r = i, i + 1
             # 같은 동안 루프문
             while l >= 0 and r < len(s) and s[l] == s[r]:
