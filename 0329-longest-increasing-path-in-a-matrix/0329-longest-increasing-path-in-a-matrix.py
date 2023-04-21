50caec933999ca9271bf42ce0fea1cd7d3fbe388
seung ju <jc3wrld999@gmail.com>
2023-04-22 02:56:01 +0900

    Time: 704 ms (9.36%), Space: 18.2 MB (40.54%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,12 +1,11 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
-        visit = []
         memo = {}
         L, M = len(matrix), len(matrix[0])
-        def move(r, c, visit, prev):
+        def move(r, c, prev):
 
-            if r not in range(L) or c not in range(M) or (r, c) in visit and matrix[r][c] <= prev:
+            if r not in range(L) or c not in range(M) or matrix[r][c] <= prev:
                 return 0
             if (r, c) in memo:
                 return memo[(r, c)]
@@ -16,18 +15,14 @@ class Solution:
             for r1, c1 in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 r2, c2 = r + r1, c + c1
 
-                visit.append((r2, c2))
-                cnt = max(cnt, 1 + move(r2, c2, visit, matrix[r][c]))
-                visit.remove((r2, c2))
+                cnt = max(cnt, 1 + move(r2, c2, matrix[r][c]))
 
             memo[(r, c)] = cnt
             return memo[(r, c)]
         max_cnt = 0
         for r in range(L):
             for c in range(M):
-                visit.append((r, c))
-                max_cnt = max(move(r, c, visit, -1), max_cnt)
-                visit.remove((r, c)) 
+                max_cnt = max(move(r, c, -1), max_cnt)
         return max_cnt
                     
         
